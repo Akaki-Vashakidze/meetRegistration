2498376fd625f94b9ef3c997faa55c785f91a5a7
akakivashakidze@Akakis-MBP.akaki-vashakidze <akakivashakidze@Akakis-MBP.akaki.vashakidze1@gmail.com>
2023-03-27 11:53:14 +0400

    fixed backend

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,14 +1,17 @@
+
+let port = 'http://localhost:3500/'
+
 export const environment = {
-    registrationURL:'http://localhost:3500/registration/register',
-    loginURL:'http://localhost:3500/login/login',
-    resultsURL:'http://localhost:3500/results/results',
-    namesURL:'http://localhost:3500/meetRegistration/names',
-    actCodesURL:'http://localhost:3500/registration/actcodes',
-    meetResultsURL:'http://localhost:3500/results/meetResults',
-    eventResultsURL:'http://localhost:3500/results/eventResults',
-    swimmerCardInfoURL:'http://localhost:3500/meetRegistration/swimmerCardInfo',
-    registerSwimmersURL:'http://localhost:3500/meetRegistration/registerSwimmers',
-    clubsURL:'http://localhost:3500/meetRegistration/clubs',
-    compsURL:'http://localhost:3500/meetRegistration/newComps',
-    doubleRegURL:'http://localhost:3500/meetRegistration/checkDoubleCompReg',
+    registrationURL: port + 'registration/register',
+    loginURL: port + 'login/login',
+    resultsURL: port + 'results/results',
+    namesURL: port + 'meetRegistration/names',
+    actCodesURL: port + 'registration/actcodes',
+    meetResultsURL: port + 'results/meetResults',
+    eventResultsURL: port + 'results/eventResults',
+    swimmerCardInfoURL: port + 'meetRegistration/swimmerCardInfo',
+    registerSwimmersURL: port + 'meetRegistration/registerSwimmers',
+    clubsURL: port + 'meetRegistration/clubs',
+    compsURL: port + 'meetRegistration/newComps',
+    doubleRegURL: port + 'meetRegistration/checkDoubleCompReg',
 };
